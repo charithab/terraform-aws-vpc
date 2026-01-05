ed306fd9580f4e8eafd295aec2bf63993cb0b1fe
charitha <charitha.harinath@gmail.com>
2026-01-05 10:43:04 +0530

    output fix

--- outputs.tf
+++ outputs.tf
@@ -7,9 +7,9 @@ output "public_subnet_ids" {
 }
 
 output "private_subnet_ids" {
-    value = aws_subnet_private[*].id
+    value = aws_subnet.private[*].id
 }
 
 output "database_subnet_ids" {
-    value = aws_subnet_database[*].id
+    value = aws_subnet.database[*].id
 }
